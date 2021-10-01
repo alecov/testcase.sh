f549cec15c53767ca3c297b1cbeeea591eae77ae
Aleksey Covacevice <alecov@mail.com>
2021-10-01 17:38:15 -0300

    Avoid `wait -p`
    Avoid using `wait -p` which requires newer Bash versions and implement
    a workaround using jobsets.

--- util.sh
+++ util.sh
@@ -22,18 +22,29 @@ extwait() {
 	waitall=($(jobpid "${waitall[@]}"))
 	waitany=($(jobpid "${waitany[@]}"))
 	waiterr=($(jobpid "${waiterr[@]}"))
-	local args=($(jobpid "$@"))
+	local index args=($(jobpid "$@"))
+	declare -A alljobs=(); for index in \
+		"${waitall[@]}" \
+		"${waitany[@]}" \
+		"${waiterr[@]}" \
+		"${args[@]}"; do
+		alljobs[$index]=1;
+	done
 	local result=0 waited wait; while true; do
 		local waited_all=
 		local waited_any=
 		local waited_err=
-		local index
 		[[ -z ${waitall[@]} ]] && return $result
-		wait=; wait -np waited \
-			"${waitall[@]}" \
-			"${waitany[@]}" \
-			"${waiterr[@]}" \
-			"${args[@]}" || wait=$?
+		wait=; wait -n "${!jobset[@]}" || wait=$?
+		declare -A livejob=(); for index in $(jobs -p); do
+			livejob[$index]=1
+		done
+		waited=; for index in ${!alljobs[@]}; do
+			if [[ -z ${livejob[$index]} ]]; then
+				waited=$index
+				unset 'alljobs[index]'
+			fi
+		done
 		[[ -z $waited ]] && return 127
 		[[ -n $wait ]] && result=$wait
 		for index in ${!args[@]}; do
