4d037902b5b20d5ea65e3c09e98941ea47c0af9e
Aleksey Covacevice <alecov@mail.com>
2021-10-01 18:33:41 -0300

    Use associative arrays as job sets
    This commit makes $waitall, $waitany and $waiterr associative arrays.
    This makes it easier to add and remove elements from them as sets.
    
    Usage is now eg. `waiterr[$!]=` instead of `waiterr+=($!)`.

--- util.sh
+++ util.sh
@@ -12,56 +12,71 @@ export -f jobpid
 
 # extwait(): waits for several jobs:
 #  All jobs in $waitall, $waitany, $waiterr and $@ are waited for.
+#  These are associative arrays used as sets.
 #  If all jobs in $waitall finish, return the last failed exit status (or zero).
 #  If any jobs in $waitany finish, return the last failed exit status (or zero).
 #  If any job in $waiterr finishes, return 127.
 #
 #  If a finished job is in both $waitall and $waitany, return an error only if
 #  the job finished with an error.
+
+declare -A waitall=()
+declare -A waitany=()
+declare -A waiterr=()
 extwait() {
-	waitall=($(jobpid "${waitall[@]}"))
-	waitany=($(jobpid "${waitany[@]}"))
-	waiterr=($(jobpid "${waiterr[@]}"))
-	local index args=($(jobpid "$@"))
-	declare -A alljobs=(); for index in \
-		"${waitall[@]}" \
-		"${waitany[@]}" \
-		"${waiterr[@]}" \
-		"${args[@]}"; do
-		alljobs[$index]=1;
+	local job
+	declare -A alljobs=()
+	for job in "$@"; do
+		job=$(jobpid "$job")
+		alljobs[$job]=
+	done
+	for job in "${!waitall[@]}"; do
+		unset 'waitall[$job]'
+		job=$(jobpid "$job")
+		waitall[$job]=
+		alljobs[$job]=
+	done
+	for job in "${!waitany[@]}"; do
+		unset 'waitany[$job]'
+		job=$(jobpid "$job")
+		waitany[$job]=
+		alljobs[$job]=
+	done
+	for job in "${!waiterr[@]}"; do
+		unset 'waiterr[$job]'
+		job=$(jobpid "$job")
+		waiterr[$job]=
+		alljobs[$job]=
 	done
 	local result=0 waited wait; while true; do
 		local waited_all=
 		local waited_any=
 		local waited_err=
-		[[ -z ${waitall[@]} ]] && return $result
-		wait=; wait -n "${!jobset[@]}" || wait=$?
-		declare -A livejob=(); for index in $(jobs -p); do
-			livejob[$index]=1
-		done
-		waited=; for index in ${!alljobs[@]}; do
-			if [[ -z ${livejob[$index]} ]]; then
-				waited=$index
-				unset 'alljobs[index]'
+		declare -A livejobs=()
+		[[ -z ${!waitall[@]} ]] && return $result
+		wait=; wait -n "${!alljobs[@]}" || wait=$?
+		for job in $(jobs -p); do livejobs[$job]=; done
+		waited=; for job in ${!alljobs[@]}; do
+			if [[ ! -v livejobs[$job] ]]; then
+				waited=$job
+				break
 			fi
 		done
 		[[ -z $waited ]] && return 127
 		[[ -n $wait ]] && result=$wait
-		for index in ${!args[@]}; do
-			[[ ${args[index]} == $waited ]] && unset 'args[index]'
-		done
-		for index in ${!waitall[@]}; do if [[ ${waitall[index]} == $waited ]]; then
+		unset 'alljobs[$waited]'
+		if [[ -v waitall[$waited] ]]; then
+			unset 'waitall[$waited]'
 			waited_all=1
-			unset 'waitall[index]'
-		fi; done
-		for index in ${!waitany[@]}; do if [[ ${waitany[index]} == $waited ]]; then
+		fi
+		if [[ -v waitany[$waited] ]]; then
+			unset 'waitany[$waited]'
 			waited_any=1
-			unset 'waitany[index]'
-		fi; done
-		for index in ${!waiterr[@]}; do if [[ ${waiterr[index]} == $waited ]]; then
+		fi
+		if [[ -v waiterr[$waited] ]]; then
+			unset 'waiterr[$waited]'
 			waited_err=1
-			unset 'waiterr[index]'
-		fi; done
+		fi
 		[[ -n $waited_err ]] && return 127
 		if [[ -n $waited_any ]]; then
 			if [[ -n $waited_all ]]; then
@@ -80,17 +95,17 @@ export -f extwait
 spawn_waitall() {
 	local name=$1; shift
 	"$@" >"$testlogdir"/"$name".out 2>"$testlogdir"/"$name".err &
-	waitall+=($!)
+	waitall[$!]=
 }
 spawn_waitany() {
 	local name=$1; shift
 	"$@" >"$testlogdir"/"$name".out 2>"$testlogdir"/"$name".err &
-	waitany+=($!)
+	waitany[$!]=
 }
 spawn_waiterr() {
 	local name=$1; shift
 	"$@" >"$testlogdir"/"$name".out 2>"$testlogdir"/"$name".err &
-	waiterr+=($!)
+	waiterr[$!]=
 }
 export -f spawn_waitall
 export -f spawn_waitany
@@ -99,11 +114,10 @@ export -f spawn_waiterr
 # semwait(): waits for a semaphore fd (a pipe) and synchronizes with all
 # processes listed for `extwait`.
 #  To use a semaphore, create a pipe and connect it to an fd:
-#    mkfifo "$tempdir"/sem
-#    exec 3<>"$tempdir"/sem
+#    exec 3<> >(:)
 semwait() {
 	local count; for ((count=0; count < ${2:-1}; ++count)); do
-		read -u${1:-3} & waitall+=($!) waitany+=($!); extwait
+		read -u${1:-3} & waitall[$!]= waitany[$!]=; extwait
 	done
 }
 export -f semwait
